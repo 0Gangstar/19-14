80298384f7244c0e6366868ebc302706ce3fae83
0Gangstar <77223822+0Gangstar@users.noreply.github.com>
2022-01-25 12:01:13 +0300

    Delete wewfseqads.tsx

--- 0.12_ Indev/maps/wewfseqads.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="home" tilewidth="64" tileheight="64" tilecount="48" columns="8">
- <image source="../data/home.png" width="512" height="384"/>
-</tileset>
